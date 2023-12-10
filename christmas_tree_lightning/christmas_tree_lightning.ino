89d263c773a24e39d67f4cb32b1f52bd9495f40f
Frank Siyli <frank.siyli@gmail.com>
2023-12-10 21:06:45 +0100

    4 led run light

--- christmas_tree_lightning/christmas_tree_lightning.ino
+++ christmas_tree_lightning/christmas_tree_lightning.ino
@@ -3,7 +3,7 @@ int LED_2 =  5 ;
 int LED_3 =  3 ; 
 int LED_4 =  9; 
 
-int Fadeschritte = 5;  
+int Fadeschritte = 50;  
 
 
 int Helligkeit = 0;  
@@ -14,9 +14,13 @@ void setup()
 void loop() 
 {
   analogWrite(LED_1, Helligkeit);
+  delay(100);
   analogWrite(LED_2, Helligkeit);
+  delay(100);
   analogWrite(LED_3, Helligkeit);
+  delay(100);
   analogWrite(LED_4, Helligkeit);
+  delay(100);
   Helligkeit = Helligkeit + Fadeschritte;    
   delay(50); 
   if (Helligkeit == 0 || Helligkeit == 255) 
