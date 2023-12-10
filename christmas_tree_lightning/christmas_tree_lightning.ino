494182c62cc937a1fd81987012dd731dacd776ba
Frank Siyli <frank.siyli@gmail.com>
2023-12-10 20:57:29 +0100

    new file

--- christmas_tree_lightning/christmas_tree_lightning.ino
+++ christmas_tree_lightning/christmas_tree_lightning.ino
@@ -1,6 +1,9 @@
-int LED = 6; 
+int LED_1 = 6; 
+int LED_2 =  5 ; 
+int LED_3 =  3 ; 
+int LED_4 =  9; 
 
-int Fadeschritte = 1;  
+int Fadeschritte = 5;  
 
 
 int Helligkeit = 0;  
@@ -10,12 +13,12 @@ void setup()
 }
 void loop() 
 {
-  
-  analogWrite(LED, Helligkeit);
- 
+  analogWrite(LED_1, Helligkeit);
+  analogWrite(LED_2, Helligkeit);
+  analogWrite(LED_3, Helligkeit);
+  analogWrite(LED_4, Helligkeit);
   Helligkeit = Helligkeit + Fadeschritte;    
-  delay(50);
-  
+  delay(50); 
   if (Helligkeit == 0 || Helligkeit == 255) 
   {
     Fadeschritte = -Fadeschritte;  
