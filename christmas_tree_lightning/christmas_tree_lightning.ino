db27b7f741a0a59fc6ca579090b85a1ed2ae06a0
Frank Siyli <frank.siyli@gmail.com>
2023-12-11 08:50:41 +0100

    4 leds fade switched on by irremote device

--- christmas_tree_lightning/christmas_tree_lightning.ino
+++ christmas_tree_lightning/christmas_tree_lightning.ino
@@ -1,31 +1,70 @@
-int LED_1 = 6; 
-int LED_2 =  5 ; 
-int LED_3 =  3 ; 
-int LED_4 =  9; 
+#include <IRremote.h>
 
-int Fadeschritte = 50;  
+int RECV_PIN = 12;
 
+int LED_1 = 6;
+int LED_2 = 5;
+int LED_3 = 10;
+int LED_4 = 9;
 
-int Helligkeit = 0;  
+int Fadeschritte = 50;
+int Helligkeit = 0;
 
-void setup()  
-{
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void setup() {
+  Serial.begin(9600);
+  irrecv.enableIRIn(); 
+  
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+  pinMode(LED_3, OUTPUT);
+  pinMode(LED_4, OUTPUT);
 }
-void loop() 
-{
+
+void fadeLEDs() {
   analogWrite(LED_1, Helligkeit);
-  delay(100);
+    delay(500);
   analogWrite(LED_2, Helligkeit);
-  delay(100);
+  delay(500);
   analogWrite(LED_3, Helligkeit);
-  delay(100);
+  delay(500);
   analogWrite(LED_4, Helligkeit);
-  delay(100);
+  delay(500);
   Helligkeit = Helligkeit + Fadeschritte;    
-  delay(50); 
-  if (Helligkeit == 0 || Helligkeit == 255) 
-  {
+  delay(5);
+  if (Helligkeit == 0 || Helligkeit == 255) {
     Fadeschritte = -Fadeschritte;  
   }
+}
 
-}  
+void toggleLEDs() {
+  if (digitalRead(LED_1) == HIGH) {
+    digitalWrite(LED_1, LOW);
+    digitalWrite(LED_2, LOW);
+    digitalWrite(LED_3, LOW);
+    digitalWrite(LED_4, LOW);
+  } else {
+    digitalWrite(LED_1, HIGH);
+    digitalWrite(LED_2, HIGH);
+    digitalWrite(LED_3, HIGH);
+    digitalWrite(LED_4, HIGH);
+  }
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+
+    if (results.value == 0xFFFFFFFF) {
+      toggleLEDs();
+    }
+
+    irrecv.resume(); 
+  }
+
+  if (digitalRead(LED_1) == HIGH) {
+    fadeLEDs();
+  }
+}
