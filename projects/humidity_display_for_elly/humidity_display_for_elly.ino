2b5a80ca603ef8a58c077ade66bc460e6dc89259
Frank Siyli <frank.siyli@gmail.com>
2023-12-11 22:36:24 +0100

    lcd1602 and dht11 are displaying temp and humidity

--- projects/humidity_display_for_elly/humidity_display_for_elly.ino
+++ projects/humidity_display_for_elly/humidity_display_for_elly.ino
@@ -1,66 +1,47 @@
-/******************************************
- *Website: www.elegoo.com
- * 
- *Time:2017.12.12
- *
- ******************************************/
-/*
-  LiquidCrystal Library - Hello World
-
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
-
- This sketch prints "Hello World!" to the LCD
- and shows the time.
-
-  The circuit:
- * LCD RS pin to digital pin 7
- * LCD Enable pin to digital pin 8
- * LCD D4 pin to digital pin 9
- * LCD D5 pin to digital pin 10
- * LCD D6 pin to digital pin 11
- * LCD D7 pin to digital pin 12
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
-
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystal
- */
-
-// include the library code:
 #include <LiquidCrystal.h>
+#include <SimpleDHT.h>
 
-// initialize the library with the numbers of the interface pins
+int pinDHT11 = 4;
+SimpleDHT11 dht11(pinDHT11);
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+byte temperature = 0;
+byte humidity = 0;
 
 void setup() {
-  // set up the LCD's number of columns and rows:
+  Serial.begin(115200);
   lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
 }
 
 void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
-}
+  Serial.println("=================================");
+  Serial.println("Sample DHT11...");
 
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT11 failed, err="); 
+    Serial.print(SimpleDHTErrCode(err));
+    Serial.print(","); 
+    Serial.println(SimpleDHTErrDuration(err));
+    delay(1000);
+    return;
+  }
+
+  Serial.print("Temperature: "); 
+  Serial.print((int)temperature); 
+  Serial.print(" *C, Humidity: "); 
+  Serial.print((int)humidity); 
+  Serial.println(" H");
+
+  delay(1500);
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: ");
+  lcd.print((int)temperature);
+  lcd.print(" C");
+
+  lcd.setCursor(0, 1);
+  lcd.print("Humidity: ");
+  lcd.print((int)humidity);
+  lcd.print(" %");
+}
