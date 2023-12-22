65ab5515f5436461c1fe0fc29365f49852cfa52f
Frank Siyli <frank.siyli@gmail.com>
2023-12-22 14:53:18 +0100

    dht11 and 16x2 lcd are working on uno r3

--- projects/humidity_display_for_elly/humidity_display_for_elly.ino
+++ projects/humidity_display_for_elly/humidity_display_for_elly.ino
@@ -1,47 +1,31 @@
 #include <LiquidCrystal.h>
-#include <SimpleDHT.h>
+#include <DHT.h>
+int pin = 6;
+int  type = DHT11;
+DHT dht (pin, type);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-int pinDHT11 = 4;
-SimpleDHT11 dht11(pinDHT11);
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
-byte temperature = 0;
-byte humidity = 0;
-
-void setup() {
-  Serial.begin(115200);
+void  setup() {
   lcd.begin(16, 2);
+   dht.begin();
 }
 
 void loop() {
-  Serial.println("=================================");
-  Serial.println("Sample DHT11...");
+ 
+ float Temp = dht.readTemperature();
+  lcd.setCursor(0,  0);
+   lcd.print("Temp:");
+    lcd.print(Temp);
+     lcd.print("C");
+     
 
-  int err = SimpleDHTErrSuccess;
-  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
-    Serial.print("Read DHT11 failed, err="); 
-    Serial.print(SimpleDHTErrCode(err));
-    Serial.print(","); 
-    Serial.println(SimpleDHTErrDuration(err));
-    delay(1000);
-    return;
-  }
+  float Humid = dht.readHumidity();
+  
 
-  Serial.print("Temperature: "); 
-  Serial.print((int)temperature); 
-  Serial.print(" *C, Humidity: "); 
-  Serial.print((int)humidity); 
-  Serial.println(" H");
+  lcd.setCursor(0, 2);
+   lcd.print("Humid:");
+    lcd.print(Humid);
+     lcd.print("%");
+               delay(2000);
 
-  delay(1500);
-
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Temp: ");
-  lcd.print((int)temperature);
-  lcd.print(" C");
-
-  lcd.setCursor(0, 1);
-  lcd.print("Humidity: ");
-  lcd.print((int)humidity);
-  lcd.print(" %");
 }
